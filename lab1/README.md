6f53ee876abc6e5f62f3c6a20f7eed20490b1ccf
Immypeko <yles.94214@gmail.com>
2024-06-28 04:47:24 +0800

    feat: add mutation-based testing on lab1

--- lab1/README.md
+++ lab1/README.md
@@ -1,19 +1,15 @@
-# Lab1
+# HW4
 
-## Introduction
+You can install package through:
 
-In this lab, you will write unit tests for functions implemented in `main.js`. You can learn how to use classes and functions in it by uncommenting the code in it.
+```shell
+npm i
+```
 
-## Requirement
+After finish your test code in `tests/calculator_test.js`, you can run `Stryker` by:
 
-1. Write test cases in `main_test.js` and achieve 100% code coverage. (100%)
+```shell
+npm run mutate
+```
 
-You can run `validate.sh` in your local to test if you satisfy the requirements.
-
-Please note that you must not alter files other than `main_test.js`. You will get 0 points if you modify other files to achieve requirements.
-
-## Submission
-
-You need to open a pull request to your branch (e.g. 311XXXXXX, your student number) and contain the code that satisfies the abovementioned requirements.
-
-Moreover, please submit the URL of your PR to E3. Your submission will only be accepted when you present at both places.
+to get your mutation testing result.
---
+++ lab1/tests/calculator_test.js
@@ -0,0 +1,60 @@
+const test = require('node:test');
+const assert = require('assert');
+const { MyClass, Student } = require('../src/calculator');
+
+const myClass = new MyClass();
+const names = ['John', 'Jane', 'Doe', 'Smith'];
+const testnames = ['John']
+
+test("Test MyClass's addStudent", () => {
+
+    class TestClass {
+        constructor() {
+            this.students = [];
+        }
+    }
+    const name = 'Timmy';
+    const student = new Student();
+    student.setName(name);
+
+    assert.strictEqual(myClass.addStudent(student), 0);
+    assert.strictEqual(myClass.addStudent(new Student()), 1);
+    assert.strictEqual(myClass.addStudent('student'), -1);
+    assert.strictEqual(myClass.addStudent(new TestClass()), -1);
+});
+
+test("Test MyClass's getStudentById", () => {
+    
+    const student1 = new Student();
+    const student2 = new Student();
+    student1.setName('Asuna');
+    const newStudentId1 = myClass.addStudent(student1);
+    student2.setName('Kirito');
+    const newStudentId2 = myClass.addStudent(student2);
+
+    assert.strictEqual(myClass.getStudentById(newStudentId1), student1);
+    assert.strictEqual(myClass.getStudentById(newStudentId2), student2);
+    assert.strictEqual(myClass.getStudentById(-1), null);
+    assert.strictEqual(myClass.getStudentById(4), null);
+});
+
+test("Test Student's setName", () => {
+    
+    const student = new Student();
+
+    assert.strictEqual(student.setName('PainPeko'), undefined);
+    assert.strictEqual(student.setName(''), undefined);
+    assert.strictEqual(student.setName(1), undefined);
+});
+
+test("Test Student's getName", () => {
+
+    const student = new Student();
+    assert.strictEqual(student.getName(), '');
+    student.setName(1);
+    assert.strictEqual(student.getName(), '');
+    student.setName('BauBau');
+    assert.strictEqual(student.getName(), 'BauBau');
+    student.setName(1);
+    assert.strictEqual(student.getName(), 'BauBau');
+});
